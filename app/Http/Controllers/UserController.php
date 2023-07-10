3f00abfd1809d2aacff512c9fc5140909de46558
MENGSRUN <85465677+MENGSRUN@users.noreply.github.com>
2023-07-10 09:14:07 +0700

    semi final

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,32 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
+
 class Usercontroller extends Controller
 {
     //
     function login(Request $req)
     {
-        $user= User::where(['email'=>$req->email])->first();
-        if(!$user || Hash::check($req->password,$user->password))
-        {
+        $user = User::where(['email' => $req->email])->first();
+        if (!$user || !Hash::check($req->password, $user->password)) {
             return "Username or password is not correct";
         }
-        else{
-            $req->session()->put('user',$user);
-            return redirect('/');
-        }
+        $req->session()->put('user', $user);
+        return redirect('/');
     }
     function register(Request $req)
     {
         // return $req->input();
         $user = new User();
-        $user->name= $req->name;
-        $user->email= $req->email;
-        $user->password=Hash::make($req->password);
-        $user ->save();
+        $user->name = $req->name;
+        $user->email = $req->email;
+        $user->password = Hash::make($req->password);
+        $user->save();
         return redirect('/login');
     }
 }
